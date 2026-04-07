6701e80e4bb884ab2b80500731e19e27e8857326
shauryauniyaal <shauryauniyal8@gmail.com>
2026-04-07 21:54:15 +0530

    Adding Schema validations

--- src/app.js
+++ src/app.js
@@ -6,20 +6,80 @@ const { default: mongoose } = require("mongoose");
 
 const app = express();
 
-app.post("/signup", (req, res) => {
-  const user = new User({
-    firstName: "Abc",
-    lastName: "Xyz",
-    emailId: "abc@xyz.com",
-    password: "abc123",
-    age: "24",
-    gender: "Male",
-  });
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
   try {
-    user.save();
+    await user.save();
     res.send("User added successfully.");
   } catch (err) {
-    res.status(400).send("User was not added.");
+    res.status(400).send(err.message || "User was not added.");
+  }
+});
+
+app.get("/users", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found.");
+    } else {
+      res.send(users);
+    }
+  } catch {
+    res.status(400).send("Something went wrong.");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("User not found.");
+    } else {
+      res.send(users);
+    }
+  } catch {
+    res.status(400).send("Something went wrong.");
+  }
+});
+
+app.get("/id", async (req, res) => {
+  const id = req.body._id;
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      res.status(404).send("User not found.");
+    } else {
+      res.send(user);
+    }
+  } catch {
+    res.status(400).send("Something went wrong.");
+  }
+});
+
+app.delete("/users", async (req, res) => {
+  const id = req.body._id;
+  try {
+    const user = await User.findByIdAndDelete(id);
+    res.send("User deleted sucessfully.");
+  } catch (error) {
+    res.status(400).send("Something went wrong.");
+  }
+});
+
+app.patch("/users", async (req, res) => {
+  const id = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(id, data, {
+      runValidators: true,
+      returnDocument: "after",
+    });
+    res.send("User updated sucessfully.");
+  } catch (error) {
+    res.status(400).send(error.message);
   }
 });
 
@@ -33,3 +93,8 @@ connectDB()
   .catch(() => {
     console.log("DB Connection unsuccessful");
   });
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,61 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      maxLength: 20,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!value.includes("@gmail.com")) {
+          throw new Error("Email id not valid.");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+      maxLength: 20,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      lowercase: true,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data not valid.");
+        }
+      },
+    },
+    photoURL: {
+      type: String,
+      default:
+        "https://thumbs.dreamstime.com/b/default-profile-picture-avatar-photo-placeholder-vector-illustration-default-profile-picture-avatar-photo-placeholder-vector-189495158.jpg",
+    },
+    about: {
+      type: String,
+      default: "This is me!",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true }, // To get createdOn and updatedOn for documents
+);
 
 const User = mongoose.model("User", userSchema);
 
