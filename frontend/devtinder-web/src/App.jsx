401001b35e75385695ba80b283e166245075e166
shauryauniyaal <shauryauniyal8@gmail.com>
2026-07-13 13:42:26 +0530

    Starting to build frontend.

--- frontend/devtinder-web/src/App.jsx
+++ frontend/devtinder-web/src/App.jsx
@@ -1,45 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+
 function App() {
   return (
     <>
-      <div className="navbar bg-base-300 shadow-md mb-5 rounded-b-2xl ">
-        <div className="flex-1">
-          <a className="btn btn-ghost font-bold text-4xl">DevTinder</a>
-        </div>
-        <div className="flex gap-2">
-          <div className="dropdown dropdown-end">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar mx-5"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
-              </div>
-            </div>
-            <ul
-              tabIndex="-1"
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ frontend/devtinder-web/src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-200 shadow-lg mb-5 rounded-b-2xl">
+      <div className="flex-1">
+        <a className="btn text-neutral btn-ghost font-bold text-4xl ">
+          DevTinder
+        </a>
+      </div>
+      <div className="flex gap-2">
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar mx-5"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Profile Image"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
